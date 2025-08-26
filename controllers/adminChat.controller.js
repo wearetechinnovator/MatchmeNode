c71ccd1be2e30cf29571db63620c5d80d444ef08
Sourav Bishai <106469372+techCodeSB@users.noreply.github.com>
2025-08-26 12:35:50 +0530

    some changes

--- controllers/adminChat.controller.js
+++ controllers/adminChat.controller.js
@@ -46,13 +46,13 @@ const get = async (req, res) => {
         let allChat
 
         // Admin only
-        if (!userData && !userId ) {
+        if (!userData && !userId) {
             allChat = await adminChatModel.find().populate("user_id").sort({ _id: -1 });
         }
-        else{
-            if(userData){
+        else {
+            if (userData) {
                 allChat = await adminChatModel.find({ user_id: userData._id }).populate("user_id").sort({ _id: -1 });
-            }else if(userId){
+            } else if (userId) {
                 allChat = await adminChatModel.find({ user_id: userId }).populate("user_id").sort({ _id: -1 });
             }
         }
@@ -119,7 +119,7 @@ const changeReadStatus = async (req, res) => {
 
 // ::::::::::::::: ADD CHAT ::::::::::::::::: 
 const addChat = async (req, res) => {
-    const { msg, msgId, msgBy } = req.body;
+    const { msg, userId, msgBy } = req.body;
 
 
     if (!msg || !msgBy) {
@@ -132,8 +132,8 @@ const addChat = async (req, res) => {
 
     try {
 
-        const updatedChat = await adminChatModel.findByIdAndUpdate(
-            msgId,
+        const updatedChat = await adminChatModel.findOneAndUpdate(
+            { user_id: userId },
             {
                 $push: {
                     message: {
@@ -142,7 +142,7 @@ const addChat = async (req, res) => {
                     }
                 }
             },
-            { new: true } // return updated document
+            { new: true } // ✅ this now works
         );
 
         if (!updatedChat) {
@@ -161,15 +161,15 @@ const addChat = async (req, res) => {
 
 // ::::::::::::::: GET CHAT ::::::::::::::::: 
 const getChat = async (req, res) => {
-    const { msgId } = req.body;
+    const { userId } = req.body;
 
 
-    if (!msgId) {
+    if (!userId) {
         return res.status(400).json({ err: "Message id required" });
     }
 
     try {
-        const chat = await adminChatModel.findById(msgId);
+        const chat = await adminChatModel.findOne({ user_id: userId });
 
         if (!chat) {
             return res.status(404).json({ err: "Chat not found" });
