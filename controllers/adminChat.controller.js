8384e1af0a3bb45d0b30aa326fdfb1fe0ac90a99
Sourav Bishai <souravbishai58558@gmail.com>
2025-08-26 11:27:08 +0530

    add chatlist controller

--- controllers/adminChat.controller.js
+++ controllers/adminChat.controller.js
@@ -46,13 +46,13 @@ const get = async (req, res) => {
         let allChat
 
         // Admin only
-        if (!userData && !userId ) {
+        if (!userData && !userId) {
             allChat = await adminChatModel.find().populate("user_id").sort({ _id: -1 });
         }
-        else{
-            if(userData){
+        else {
+            if (userData) {
                 allChat = await adminChatModel.find({ user_id: userData._id }).populate("user_id").sort({ _id: -1 });
-            }else if(userId){
+            } else if (userId) {
                 allChat = await adminChatModel.find({ user_id: userId }).populate("user_id").sort({ _id: -1 });
             }
         }
@@ -81,6 +81,29 @@ const get = async (req, res) => {
     }
 
 }
+
+// :::::::::::::::::::::::::::: GET CHAT LIST ::::::::::::::::::::::::::::
+const getList = async (req, res) => {
+    try {
+        const chats = await adminChatModel
+            .find()
+            .populate("user_id")
+            .sort({ _id: -1 });
+
+        const list = chats.map(chat => {
+            const lastMessage = chat.message.length > 0 ? chat.message[chat.message.length - 1] : null;
+            return {
+                user: chat.user_id,
+                lastMessage
+            };
+        });
+
+        return res.status(200).json({list});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ err: "Something went wrong" });
+    }
+};
 
 
 // ::::::::::::::::::::::::::::: CHANGE READ STATUS ::::::::::::::::::::::::
@@ -191,5 +214,6 @@ module.exports = {
     add, get,
     changeReadStatus,
     addChat,
-    getChat
+    getChat,
+    getList
 }
